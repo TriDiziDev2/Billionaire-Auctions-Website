461f013ece2f42ce1c3bb04e476cd7ad77be916e
NarahariVasantham1567 <naraharivasantham1567@gmail.com>
2026-02-06 13:29:21 +0530

    working on but and sell items

---
+++ src/components/RealEstate/RealEstate.jsx
@@ -0,0 +1,82 @@
+import './RealEstate.css';
+
+import { LuHouse } from 'react-icons/lu';
+import { FaAngleLeft } from 'react-icons/fa6';
+import { FaAngleRight } from 'react-icons/fa6';
+import { RxCross2 } from 'react-icons/rx';
+
+import heritage from '../../assets/heritage.jpg';
+import apartment from '../../assets/apartment.jpg';
+
+import RealEstateCard from '../RealEstateCard/RealEstateCard';
+
+const realEstateData = [
+  {
+    id: 1,
+    title: 'Heritage Villa - Gated Community',
+    image: heritage,
+    cost: '28,50,00,000',
+    city: 'Gurgaon, Haryana',
+    location: 'Gurgoan',
+  },
+  {
+    id: 2,
+    title: 'Beachfront Villa - Goa',
+    image: apartment,
+    cost: '18,75,00,000',
+    city: 'Goa',
+    location: 'Goa',
+  },
+  {
+    id: 3,
+    title: 'Modern Apartment - City Center',
+    image: apartment,
+    cost: '12,50,00,000',
+    city: 'Bangalore, Karnataka',
+    location: 'Bangalore',
+  },
+  {
+    id: 4,
+    title: 'Colonial Mansion',
+    image: apartment,
+    cost: '35,00,00,000',
+    city: 'Kolkata, West Bengal',
+    location: 'Kolkata',
+  },
+];
+
+const RealEstate = () => {
+  return (
+    <div className='featured-listings-container'>
+      <div className='featured-listings-header'>
+        <div className='featured-listings-heading-container'>
+          <div className='real-estate-icon-container'>
+            <LuHouse />
+          </div>
+          <div className='real-estate-content-container'>
+            <h3 className='featured-listings-heading'>Real Estate</h3>
+            <p className='real-estate-text'>5 Products Available</p>
+          </div>
+        </div>
+        <div className='featured-listings-arrow-btn-container'>
+          <button className='arrow-btn'>
+            <FaAngleLeft />
+          </button>
+          <button className='arrow-btn'>
+            <FaAngleRight />
+          </button>
+          <button className='real-estate-btn'>
+            <RxCross2 />
+          </button>
+        </div>
+      </div>
+      <div className='featured-listings-grid-container'>
+        {realEstateData.map((item) => (
+          <RealEstateCard key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RealEstate;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,9 @@
+import BillionaireAuction from '../../components/BillionaireAuctions/BillionaireAuction';
+import BrowseByCategory from '../../components/BrowseByCategory/BrowseByCategory';
+import BuyAndSell from '../../components/BuyAndSell/BuyAndSell';
 import FeaturedListings from '../../components/FeaturedListings/FeaturedListings';
 import HeroBanner from '../../components/HeroBanner/HeroBanner';
+import RealEstate from '../../components/RealEstate/RealEstate';
 import Recommendations from '../../components/Recommendations/Recommendations';
 import './HomePage.css';
 
@@ -9,6 +13,10 @@ const HomePage = () => {
       <HeroBanner />
       <FeaturedListings />
       <Recommendations />
+      <BrowseByCategory />
+      <RealEstate />
+      <BillionaireAuction />
+      <BuyAndSell />
     </div>
   );
 };
